892b2c7c4c06394c549f0e8e6b11a78d3fc7279e
zaland <sanikhankhan749@gmail.com>
2026-01-08 04:23:09 -0800

    fixed cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://your-frontend-domain.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
